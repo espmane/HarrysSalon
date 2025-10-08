5930a26df950097f6936c974189391d7c5c19e16
Esp <duckerlicious@duck.com>
2025-10-08 13:21:51 +0200

    .

--- src/HarrysSalon/HarrySalon.java
+++ src/HarrysSalon/HarrySalon.java
@@ -7,44 +7,49 @@ public class HarrySalon {
     Scanner scanner = new Scanner(System.in);
 
     public void menu() {
-        showMenu();
-        int choise = 0;
-        switch (choise) {
-            case 1:
-                addTime();
-                break;
-            case 2:
-                removeTime();
-                break;
-            case 3:
-                editTime();
-                break;
-            case 4:
-                buyAddictional();
-                break;
-            case 5:
-                showAppointments();
-                break;
-
+        boolean exit = false;
+        System.out.println("""
+                    valid options:
+                    1. add a new appointment 'add'
+                    2. remove a appointment 'remove'
+                    3. edit appointment  'edit'
+                    4. addictional buys 'buy'
+                    5. show all appointments 'show'
+                    Go back "back"
+                    """);
+        while (exit) ;
+        {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    addTime();
+                    break;
+                case 2:
+                    removeTime();
+                    break;
+                case 3:
+                    editTime();
+                    break;
+                case 4:
+                    buyAddictional();
+                    break;
+                case 5:
+                    showAppointments();
+                    break;
+                case 0:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid input, try again.");
+            }
         }
     }
 
-    public void showMenu(){
-        System.out.println("""
-                valid options:
-                1. add a new appointment 'add'
-                2. remove a appointment 'remove'
-                3. edit appointment  'edit'
-                4. addictional buys 'buy'
-                5. show all appointments 'show'
-                Go back "back"
-                """);
-    }
 
     public void addTime(){
-
         Cuts cuts = new Cuts();
-        this.appointments.add(cuts);
+
+        appointments.add(cuts);
         System.out.println("You've added: " + appointments + " ");
     }
 
